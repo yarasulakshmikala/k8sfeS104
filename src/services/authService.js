61f53126cdbc0a054eb26d68d2202efbee5769e2
yarasulakshmikala <143312838+yarasulakshmikala@users.noreply.github.com>
2025-10-31 21:20:33 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -9,7 +9,7 @@ export const login = async (username, password) => {
 };
 
 export const signup = async (username, email, password) => {
-  return axios.post(`${API_URL}/sign`, { username, email, password });
+  return axios.post(`${API_URL}/signup`, { username, email, password });
 };
 
 export const logout = () => {
